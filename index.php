0bb3ebeb8b8a13b9148b2e035bca2c1f100b76ca
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-02-09 20:47:43 -0300

    Contador em PHP

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php 
+//2 ARRAYS E VAMOS VERIFICAR OS NÚMEROS EM COMUM.
+
+$array_0 = array(0,1,3,4,6,8);
+$array_1 = array(10,90,23,8,6);
 
 
+//Rodar um looping e verificar se existe em um e no outro
 
-
+$contador = 0;
+for($i = 0; $i < count($array_0); $i++){
+	for($j = 0; $j < count($array_1); $j++){
+		$contador++;
+		echo $contador;
+		echo "<br>";
+	}
+}
 
 ?>
