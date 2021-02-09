56ccb42fb02c85a1420386a757aeb40dc316aefa
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-02-09 20:55:52 -0300

    Comparar 2 array e retorna os valores iguais em ambos.

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php 
 //2 ARRAYS E VAMOS VERIFICAR OS NÚMEROS EM COMUM.
 
-$array_0 = array(0,1,3,4,6,8);
-$array_1 = array(10,90,23,8,6);
+$array_0 = array(0,1,3,4,6,8, 'Ilton');
+$array_1 = array(10,90,23,8,6, 'Ilton');
 
 
 //Rodar um looping e verificar se existe em um e no outro
 
-$contador = 0;
+$em_comum = [];
 for($i = 0; $i < count($array_0); $i++){
 	for($j = 0; $j < count($array_1); $j++){
-		$contador++;
-		echo $contador;
-		echo "<br>";
+		if($array_0[$i] === $array_1[$j]){
+			//numero em comum
+			$em_comum[] = $array_0[$i];
+		}
 	}
+}
+
+foreach ($em_comum as  $key => $value) {
+	# code...
+	echo $value;
+	echo "<br>";
 }
 
 ?>
