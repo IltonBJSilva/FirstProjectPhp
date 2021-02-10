3805a71316b2b002325cbe152fa8ed5c5e771b49
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-02-09 21:31:40 -0300

    Mostra a quantidade de vezes que um valor se repete dentro de um array

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <?php 
 //2 ARRAYS E VAMOS VERIFICAR OS NÚMEROS EM COMUM.
 
-$array_0 = array(0,1,3,4,6,8, 'Ilton');
-$array_1 = array(10,90,23,8,6, 'Ilton');
+$array = array('Ilton', 'Marcos', 'Vinicius', 'Ilton', 'Claudio','Kesley');
+$arrayRepetido = array();
 
+// Rodar um loop para verificar os valores repetidos dentro do array
+for($i = 0; $i < count($array); $i++){
+	$valorAtual = $array[$i];
+	if(!isset($arrayRepetido[$valorAtual])){
+		$arrayRepetido[$valorAtual] = 0;
+	} else {
+		# code...
+		$arrayRepetido[$valorAtual]++;
+	}
 
-//Rodar um looping e verificar se existe em um e no outro
-
-$em_comum = [];
-for($i = 0; $i < count($array_0); $i++){
-	for($j = 0; $j < count($array_1); $j++){
-		if($array_0[$i] === $array_1[$j]){
-			//numero em comum
-			$em_comum[] = $array_0[$i];
-		}
+	foreach ($arrayRepetido as $key => $value) {
+		# code...
+		echo $key;
+		echo $value;
+		echo "<hr>";
 	}
 }
 
-foreach ($em_comum as  $key => $value) {
-	# code...
-	echo $value;
-	echo "<br>";
-}
+
+
 
 ?>
---
+++ old/index3.php
@@ -0,0 +1,27 @@
+<?php 
+//2 ARRAYS E VAMOS VERIFICAR OS NÚMEROS EM COMUM.
+
+$array_0 = array(0,1,3,4,6,8, 'Ilton');
+$array_1 = array(10,90,23,8,6, 'Ilton');
+
+
+
+//Rodar um looping e verificar se existe em um e no outro
+
+$em_comum = [];
+for($i = 0; $i < count($array_0); $i++){
+	for($j = 0; $j < count($array_1); $j++){
+		if($array_0[$i] === $array_1[$j]){
+			//numero em comum
+			$em_comum[] = $array_0[$i];
+		}
+	}
+}
+
+foreach ($em_comum as  $key => $value) {
+	# code...
+	echo $value;
+	echo "<br>";
+}
+
+?>
