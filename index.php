dd1f3b079e1bfc5a9bad329ccf17bd73bbca2b65
Ilton Batista da Silva Júnior <xlsonplay@gmail.com>
2021-02-14 20:30:42 -0300

    Gera numeros randomicos sem repetir

--- index.php
+++ index.php
@@ -4,11 +4,28 @@
 //Preencher array sem número repetidos.
 $arr = [];
 
-for($i = 0; $i < 5; $i++){
+for($i = 0; $i < 2; $i++){
 	//gerar um numero aleatorio entre 1 e 5
-	$arr[$i] = rand(1,20);
-	echo $arr[$i];
-	echo "<br>";
+	$arr[$i] = rand(1,2);
+	//Em quanto existir no meu array o %arr no indice $i e o ar novamente
+	while (inArrayCustom($i,$arr[$i], $arr)) {
+		# code...
+		$arr[$i] = rand(1,5);
+
+	}
 }
+
+print_r($arr);
+	function inArrayCustom($indice, $valor, $arr){
+		for($i = 0; $i < count($arr); $i++){
+			if($arr[$i] == $valor && $i != $indice){
+				return true;
+			}
+	
+		}
+
+		return false;
+
+	} 
 
 ?>
